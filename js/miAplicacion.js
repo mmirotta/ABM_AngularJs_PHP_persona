99db925b362bbd21bbb929d57ef60663b90f39e3
mmirotta <m.mirotta@gmail.com>
2016-09-08 21:03:31 -0300

    estados, indice, menu principal, vistas

---
+++ js/miAplicacion.js
@@ -0,0 +1,81 @@
+var miApp = angular.module('angularABM', ['ui.router']);
+
+miApp.config(function($stateProvider, $urlRouterProvider){
+	//aca se genera el ruteo atravez de estados
+
+	$stateProvider
+		.state(
+			"inicio",
+			{
+				url:"/inicio",
+				templateUrl:"inicio.html",
+				controller:"controlInicio"
+			}
+		)
+
+		.state(
+			"persona",
+			{
+				url:"/persona",
+				abstract:true, //
+				templateUrl:"abstractaPersona.html"
+			}
+		)
+
+		.state(
+			"persona.menu",
+			{
+				url:"/menu",
+				views:{
+					"contenido": {
+					templateUrl:"personaMenu.html",
+					controller:"controlPersonaMenu"
+					}
+				}
+			}
+		)
+
+		.state(
+			"persona.alta",
+			{
+				url:"/alta",
+				views:{
+					"contenido": {
+					templateUrl:"personaAlta.html",
+					controller:"controlPersonaAlta"
+					}
+				}
+			}
+		)
+
+		.state(
+			"persona.grilla",
+			{
+				url:"/grilla",
+				views:{
+					"contenido": {
+					templateUrl:"personaGrilla.html",
+					controller:"controlPersonaGrilla"
+					}
+				}
+			}
+		)
+
+	$urlRouterProvider.otherwise("/persona/grilla");
+});
+
+miApp.controller("controlInicio", function($scope) {
+
+});
+
+miApp.controller("controlPersonaMenu", function($scope) {
+
+});
+
+miApp.controller("controlPersonaAlta", function($scope) {
+
+});
+
+miApp.controller("controlPersonaGrilla", function($scope) {
+
+});
