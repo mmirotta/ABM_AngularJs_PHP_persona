be29b5745018684461438ec05125295af4d369ce
matias mirotta <m.mirotta@gmail.com>
2016-09-12 21:35:36 -0300

    terminados todos los juegos y los accesos a las 3 aplicaciones.

--- js/miAplicacion.js
+++ js/miAplicacion.js
@@ -255,49 +255,376 @@ miApp.controller("controlJuegosMenu", function($scope) {
 });	
 
 miApp.controller("controlAdivinaUno", function($scope) {
+	$scope.random = "";
+	$scope.intentos = "0";
+	$scope.resultado = "";
+
 	$scope.comenzar = function(){
-		controle.log("comienza el juego");
+		$scope.random = Math.floor((Math.random() * 100) + 1);
 	};
 
 	$scope.verificar = function(){
-		controle.log("comienza el juego");
+		$scope.intentos = parseInt($scope.intentos) + 1;
+		if (parseInt($scope.numero) > parseInt($scope.random))
+		{
+			$scope.resultado = "Fallaste. Te pasaste del numero secreto.";
+		}
+		else
+		{
+			if (parseInt($scope.numero) < parseInt($scope.random))
+			{
+				$scope.resultado = "Fallaste. Te falta para el numero secreto.";
+				
+			}
+			else
+			{
+				$scope.resultado = "Usted es un ganador!! y en solo " + $scope.intentos +" intentos.";
+				$scope.random = "";
+			}
+		}
+		$scope.colorResultado = getRandomColor();
 	};
+
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF';
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+    	return color;
+	}
 });	
 
 miApp.controller("controlAdivinaDos", function($scope) {
+	$scope.random = "";
+	$scope.intentos = "0";
+	$scope.resultado = "";
+
 	$scope.comenzar = function(){
-		controle.log("comienza el juego");
+		$scope.random = Math.floor((Math.random() * 100) + 1);
 	};
 
 	$scope.verificar = function(){
-		controle.log("comienza el juego");
+		$scope.intentos = parseInt($scope.intentos) + 1;
+		if (parseInt($scope.numero) > parseInt($scope.random))
+		{
+			$scope.resultado = "Fallaste. Te pasaste del numero secreto.";
+		}
+		else
+		{
+			if (parseInt($scope.numero) < parseInt($scope.random))
+			{
+				$scope.resultado = "Fallaste. Te falta para el numero secreto.";
+				
+			}
+			else
+			{
+				switch(parseInt($scope.intentos))
+				{
+					case 1:
+							$scope.resultado = "Usted es un Psiquico.";
+						break;
+					case 2:
+							$scope.resultado = "Excelente percepcion.";
+						break;
+					case 3:
+							$scope.resultado = "Esto es suerte.";
+						break;
+					case 4:
+							$scope.resultado = "Excelente tecnica.";
+						break;
+					case 5:
+							$scope.resultado = "Usted esta en la media.";
+						break;
+					case 6:
+					case 7:
+					case 8:
+					case 9:
+							$scope.resultado = "Falta tecnica.";
+						break;
+					default:
+							$scope.resultado = "Afortunado en el amor!!";
+						break;
+				}
+			}
+		}
+		$scope.colorResultado = getRandomColor();
 	};
+
+	function getRandomColor() {
+	    var letters = '0123456789ABCDEF';
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+    	return color;
+	}
 });
 
 miApp.controller("controlAgilidadUno", function($scope) {
-	$scope.comenzar = function(){
-		controle.log("comienza el juego");
-	};
+
+	$scope.primerNumero = Math.floor((Math.random() * 10) + 1);
+	$scope.segundoNumero = Math.floor((Math.random() * 10) + 1);
+	$scope.imagen = "signo.png";
+	var random = Math.floor((Math.random() * 100) + 1);
+
+	if (random < 15)
+	{
+		$scope.operador = "-";
+	}
+	else if ((random > 15) && (random <= 45))
+	{
+		$scope.operador = "+";
+	}
+	else if ((random > 45) && (random <= 75))
+	{
+		$scope.operador = "/";
+	}
+	else if ((random > 75) && (random <= 100))
+	{
+		$scope.operador = "*";
+	}
+
 
 	$scope.responder = function(){
-		controle.log("comienza el juego");
+		var resultado = "";
+		switch ($scope.operador)
+		{
+			case "-":
+				resultado = parseInt($scope.primerNumero) - parseInt($scope.segundoNumero);
+				break;
+			case "+":
+				resultado = parseInt($scope.primerNumero) + parseInt($scope.segundoNumero);
+				break;
+			case "/":
+				resultado = parseInt($scope.primerNumero) / parseInt($scope.segundoNumero);
+				break;
+			case "*":
+				resultado = parseInt($scope.primerNumero) * parseInt($scope.segundoNumero);
+				break;
+		}
+
+		if (parseInt($scope.respuesta) == parseInt(resultado))
+		{
+			console.log("correcto");
+			$scope.imagen = "check.png";
+		}
+		else
+		{
+			console.log("error");
+			$scope.imagen = "error.png";
+		}
 	};
 });	
 
 miApp.controller("controlAgilidadDos", function($scope) {
-	$scope.comenzar = function(){
-		controle.log("comienza el juego");
+	$scope.primerNumero = Math.floor((Math.random() * 10) + 1);
+	$scope.segundoNumero = Math.floor((Math.random() * 10) + 1);
+	$scope.imagen = "signo.png";
+	var random = Math.floor((Math.random() * 100) + 1);
+
+	if (random < 15)
+	{
+		$scope.operador = "-";
+	}
+	else if ((random > 15) && (random <= 45))
+	{
+		$scope.operador = "+";
+	}
+	else if ((random > 45) && (random <= 75))
+	{
+		$scope.operador = "/";
+	}
+	else if ((random > 75) && (random <= 100))
+	{
+		$scope.operador = "*";
+	}
+	timeout();
+	$scope.responder = function(){
+		verificar();
 	};
 
-	$scope.responder = function(){
-		controle.log("comienza el juego");
-	};
+	function timeout(){
+	    setTimeout(function(){
+	        verificar();
+	    },4000,"JavaScript");
+	}
+
+    function verificar()
+    {
+    	var resultado = "";
+		switch ($scope.operador)
+		{
+			case "-":
+				resultado = parseInt($scope.primerNumero) - parseInt($scope.segundoNumero);
+				break;
+			case "+":
+				resultado = parseInt($scope.primerNumero) + parseInt($scope.segundoNumero);
+				break;
+			case "/":
+				resultado = parseInt($scope.primerNumero) / parseInt($scope.segundoNumero);
+				break;
+			case "*":
+				resultado = parseInt($scope.primerNumero) * parseInt($scope.segundoNumero);
+				break;
+		}
+
+		if (parseInt($scope.respuesta) == parseInt(resultado))
+		{
+			console.log("correcto");
+			$scope.imagen = "check.png";
+		}
+		else
+		{
+			console.log("error");
+			$scope.imagen = "error.png";
+		}
+    }
 });
 
 miApp.controller("controlPiedraPapelTijeraUno", function($scope) {
+	comenzar();
 
+	$scope.piedra = function(){
+		verificar("piedra");
+	};
+	$scope.papel = function(){
+		verificar("papel");
+	};
+	$scope.tijera = function(){
+		verificar("tijera");
+	};
+
+	function verificar(eleccion){
+		switch (eleccion)
+		{
+			case "piedra":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Empate";
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Usted Pierde";
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Usted Gana";
+					} 
+				break;
+			case "papel":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Usted Gana";
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Empate";
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Usted Pierde";
+					} 
+				break;
+			case "tijera":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Usted Pierde";
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Usted Gana";
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Empate";
+					} 
+				break;
+		}
+		comenzar();
+	}
+
+	function comenzar(){
+		$scope.maquina = Math.floor((Math.random() * 3) + 1);
+	}
 });
 
 miApp.controller("controlPiedraPapelTijeraDos", function($scope) {
+	comenzar();
 
+	$scope.ganadas = "0";
+	$scope.empatadas = "0";
+	$scope.perdidas = "0";
+
+	$scope.piedra = function(){
+		verificar("piedra");
+	};
+	$scope.papel = function(){
+		verificar("papel");
+	};
+	$scope.tijera = function(){
+		verificar("tijera");
+	};
+
+	function verificar(eleccion){
+		switch (eleccion)
+		{
+			case "piedra":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Empate";
+						$scope.empatadas = parseInt($scope.empatadas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Usted Pierde";
+						$scope.perdidas = parseInt($scope.perdidas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Usted Gana";
+						$scope.ganadas = parseInt($scope.ganadas) + 1;
+					} 
+				break;
+			case "papel":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Usted Gana";
+						$scope.ganadas = parseInt($scope.ganadas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Empate";
+						$scope.empatadas = parseInt($scope.empatadas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Usted Pierde";
+						$scope.perdidas = parseInt($scope.perdidas) + 1;
+					} 
+				break;
+			case "tijera":
+					if (parseInt($scope.maquina) == 1)
+					{
+						$scope.resultado = "Usted Pierde";
+						$scope.perdidas = parseInt($scope.perdidas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 2)
+					{
+						$scope.resultado = "Usted Gana";
+						$scope.ganadas = parseInt($scope.ganadas) + 1;
+					} 
+					else if (parseInt($scope.maquina) == 3)
+					{
+						$scope.resultado = "Empate";
+						$scope.empatadas = parseInt($scope.empatadas) + 1;
+					} 
+				break;
+		}
+		comenzar();
+	}
+
+	function comenzar(){
+		$scope.maquina = Math.floor((Math.random() * 3) + 1);
+	}
 });
